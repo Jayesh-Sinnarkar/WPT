5c2540ba97c2f2378ae62ecb60bdb6a57cc020fd
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-04 00:35:16 +0530

    Min of Array Uploaded
    Dynamic input implemented

---
+++ ASSIGNMENTS/Java Script Assignments/7/7_minimumOfTheNumbers.js
@@ -0,0 +1,29 @@
+var arr=[]
+var arrSize
+
+const displayForm = ()=>{
+
+    arrSize = parseInt(document.getElementById("no").value) 
+
+    for(var i=1;i<=arrSize;i++) {
+    document.getElementById("output").innerHTML += 
+    "<label for=\"no\">Enter No"+i+":</label> &nbsp;&nbsp; <input type=\"text\" name=\"num\"><br><br>"
+    }
+    document.getElementById("output").innerHTML += "<button type=\"button\" id=\"btn2\" onclick=\"getArray()\">Display Array</button> &nbsp; <button type=\"button\" id=\"btn3\" onclick=\"getMin()\">Get Minimum</button>"
+
+}
+
+const getArray = ()=>{
+    
+    for(var i=0;i<arrSize;i++){
+        
+        arr[i] = document.getElementsByName("num")[i].value
+    }
+
+    document.getElementById("output").innerHTML += "<br><br><br> Input Array  ["+arr+"] <br><br>"
+}
+
+const getMin = ()=>{
+    var min = arr.reduce((acc,val)=>acc<val?acc:val)
+    document.getElementById("output").innerHTML += "Minimum Value is: "+min+"<br><br>"
+}
