e3eee82f275eeed0615cf0cb50edd0316787613d
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-04 01:52:35 +0530

    Completed MinNum Assignment
    Completed MinNum Assignment

--- ASSIGNMENTS/Java Script Assignments/7/7_minimumOfTheNumbers.js
+++ ASSIGNMENTS/Java Script Assignments/7/7_minimumOfTheNumbers.js
@@ -9,21 +9,23 @@ const displayForm = ()=>{
     document.getElementById("output").innerHTML += 
     "<label for=\"no\">Enter No"+i+":</label> &nbsp;&nbsp; <input type=\"text\" name=\"num\"><br><br>"
     }
-    document.getElementById("output").innerHTML += "<button type=\"button\" id=\"btn2\" onclick=\"getArray()\">Display Array</button> &nbsp; <button type=\"button\" id=\"btn3\" onclick=\"getMin()\">Get Minimum</button>"
-
+    document.getElementById("output").innerHTML += "&nbsp; <button type=\"button\" id=\"btn3\" onclick=\"getMin()\">Get Minimum</button>"
 }
+
 
 const getArray = ()=>{
     
     for(var i=0;i<arrSize;i++){
-        
-        arr[i] = document.getElementsByName("num")[i].value
+
+        arr[i] = parseInt(document.getElementsByName("num")[i].value)
     }
 
-    document.getElementById("output").innerHTML += "<br><br><br> Input Array  ["+arr+"] <br><br>"
+    document.getElementById("output").innerHTML += "<br><br><br> Input Array is ["+arr+"] <br><br>"
 }
 
 const getMin = ()=>{
+    getArray();
     var min = arr.reduce((acc,val)=>acc<val?acc:val)
     document.getElementById("output").innerHTML += "Minimum Value is: "+min+"<br><br>"
+    document.getElementById("no").value=""
 }
