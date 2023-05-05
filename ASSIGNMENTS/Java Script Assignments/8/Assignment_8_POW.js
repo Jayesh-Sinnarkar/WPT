41ba85640acf4905908cdf7c7d9c4ead0727da80
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-06 01:06:49 +0530

    Uploading JS Assignment 8
    Concluding  JS Assignment 8

---
+++ ASSIGNMENTS/Java Script Assignments/8/Assignment_8_POW.js
@@ -0,0 +1,31 @@
+var num
+var pow
+const displayArea = ()=> {
+
+    document.getElementById("resDiv").innerHTML = ""
+    
+    num = parseInt(document.getElementById("num").value)
+    pow = parseInt(document.getElementById("pow").value)
+    var inDiv = document.getElementById("resDiv")
+    
+    inDiv.style.display = "block" 
+
+    document.getElementById("resDiv").innerHTML += "<span id=\"span\">Result of  ("+num+") <sup> "+pow+" </sup>is "+calculatePow()+"</span>";
+
+    
+
+}
+
+const calculatePow = () => {
+    var res = 1;
+    document.getElementById("num").value = ""
+    document.getElementById("pow").value = ""
+
+    for(var i=1;i<=pow;i++)
+    {
+        res = res*num
+        
+    }
+
+    return res;
+}
