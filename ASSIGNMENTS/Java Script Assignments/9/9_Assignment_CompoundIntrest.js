5365d69c3b2ca196876f8f1b2913c0e558a6455b
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-07 01:46:33 +0530

    Uploading assignment 9
    Uploading assignment 9

--- ASSIGNMENTS/Java Script Assignments/9/9_Assignment_CompoundIntrest.js
+++ ASSIGNMENTS/Java Script Assignments/9/9_Assignment_CompoundIntrest.js
@@ -1,7 +1,18 @@
 const calCompound = ()=> {
-    var p = 
-    var r
-    var n
+    var p = parseInt(document.getElementById("pri").value)
+    var r = parseFloat(document.getElementById("roi").value)
+    var n = parseFloat(document.getElementById("yrs").value)
+    var ci = 0
 
-    //Calculate CI
+    if(isNaN(p)==true)
+    {
+        
+    }
+
+    ci= parseFloat((Math.pow(1+(r/100),n)*p)-p).toFixed(2)
+    
+    document.getElementById("out").style.display = "block"
+    
+    document.getElementById("out").innerHTML = " <td  id=\"output\" align=\"center\" colspan=\"2\">Compound Intrest = "+ci+"</td>"
+
 }
