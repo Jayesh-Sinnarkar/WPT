5359047abdfcf41ad1078f6179d272124a0b5f44
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-08 10:22:22 +0530

    Adding Assignment 12

--- ASSIGNMENTS/Java Script Assignments/9/9_Assignment_CompoundIntrest.js
+++ ASSIGNMENTS/Java Script Assignments/9/9_Assignment_CompoundIntrest.js
@@ -3,11 +3,11 @@ const calCompound = ()=> {
     var r = parseFloat(document.getElementById("roi").value)
     var n = parseFloat(document.getElementById("yrs").value)
     var ci = 0
-
-    if(isNaN(p)==true)
-    {
-        
-    }
+    
+    clearMessages()
+    
+    if(validations(p,r,n))
+    { 
 
     ci= parseFloat((Math.pow(1+(r/100),n)*p)-p).toFixed(2)
     
@@ -15,4 +15,36 @@ const calCompound = ()=> {
     
     document.getElementById("out").innerHTML = " <td  id=\"output\" align=\"center\" colspan=\"2\">Compound Intrest = "+ci+"</td>"
 
+    }
+}
+
+const clearMessages = () =>
+{
+    document.getElementById("errp").style.display = "none"
+    document.getElementById("errr").style.display = "none"
+    document.getElementById("erry").style.display = "none"
+}
+
+const validations = (p,r,n) => {
+
+    if(isNaN(p))
+    {
+        document.getElementById("errp").style.display = "block"
+        return false;
+
+    }
+    else if(isNaN(r))
+    {
+        document.getElementById("errr").style.display = "block"
+        return false;
+
+    }
+    else if(isNaN(n))
+    {
+        document.getElementById("erry").style.display = "block"
+        return false;
+
+    }
+
+    return true;
 }
