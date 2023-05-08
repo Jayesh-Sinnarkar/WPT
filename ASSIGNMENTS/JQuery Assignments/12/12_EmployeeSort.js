41ce594e3da6eb7e860c155cd8166879c6520b90
jayesh <jayesh.sinnarkar@gmail.com>
2023-05-08 14:50:09 +0530

    concluding question13 in Jquery
    concluding question13 in Jquery: added sorting mechanism as instructed

--- ASSIGNMENTS/JQuery Assignments/12/12_EmployeeSort.js
+++ ASSIGNMENTS/JQuery Assignments/12/12_EmployeeSort.js
@@ -1,17 +1,23 @@
 var empName = [];
 
 $(document).ready(function(){
-    
-    
-    
-    
-    
+
 $('#btn').click(function(){
 
-
-
+for(var i=0; i<6; i++)
+{
+    empName[i]=document.getElementsByName("ename")[i].value
+}
+sortEmpData(empName)
 })
 
+const sortEmpData = (empName) =>{
+    var res = empName.sort()
+    $('#output').css("display","block")
+
+    $('span').html("Names in Sorted Order:"+res)
+    
+}
 
 
 
